85c6b47215664abe31316514ecb9c6e49c6f5ed5
Lucio Gonzalez <lgonzalez_94@hotmail.com>
2020-05-16 15:38:01 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -4,7 +4,7 @@ def fizzbuzz(num)
   if num % 3 == 0
     puts "Fizz"
     "Fizz"
-  elsif num / 5 == 1 
+  elsif num % 5 == 0 
     puts "Buzz"
   elsif (num / 3) && (num / 5) == 1
     puts "FizzBuzz"
